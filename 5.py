9aec6f00e058dbc6fa664525be58154175cd1186
hanlixiao <439840469@qq.com>
2026-03-09 19:48:35 +0800

    first save

--- 5.py
+++ 5.py
@@ -1,3 +1,5 @@
+import re
+
 medical_records = [
     {
         'patient_id': 'P1001',
@@ -33,7 +35,17 @@ medical_records = [
     }
 ]
 
-
+def find_invalid_records(
+    patient_id, age, gender, diagnosis, medications, last_visit_id
+):
+    constraints = {
+        'patient_id': isinstance(patient_id, str) and re.fullmatch('p\d+', patient_id, re.IGNORECASE),
+        'age': isinstance(age, int) and age >= 18,
+        'gender': isinstance(gender, str) and gender.lower() in ('male', 'female'),
+        'diagnosis': isinstance(diagnosis, str) or diagnosis is None,
+        'medications': isinstance(medications, list)
+    }
+    return constraints
 
 def validate(data):
     is_sequence = isinstance(data, (list, tuple))
@@ -64,3 +76,4 @@ def validate(data):
     return True
 
 validate(medical_records)
+print(find_invalid_records(**medical_records[0]))
