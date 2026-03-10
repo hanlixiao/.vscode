07ee5472e764f135053a8a8e69f30fc3b211a0ce
hanlixiao <439840469@qq.com>
2026-03-10 14:05:31 +0800

    first save

--- 6.py
+++ 6.py
@@ -1,4 +1,5 @@
 test_seettings = {'theme': 'dark', 'notifications': 'enabled', 'volume': 'high'}
 
 def add_setting(dictionary, tupel):
-    dictionary.item().lower()
+    key = tuple[0].lower()
+    value = tuple[1].lower()
