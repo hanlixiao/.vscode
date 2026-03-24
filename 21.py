ef57427bf9670cea4a1194684cdbcb332607ef2a
hanlixiao <439840469@qq.com>
2026-03-24 11:40:56 +0800

    Frist save

--- 21.py
+++ 21.py
@@ -1,18 +1,22 @@
+#练习         使用二分法查找数字平方根的函数
 
 
-
-
+#定义函数
 def square_root_bisection(square_target, tolerance = 1e-6, max_iterations = 100):
+    #判断所求值是否大于0
     if square_target < 0:
         raise ValueError("Square root of negative number is not defined in real numbers")
     
+    #如果传入的值为0或1则返回本身
     if square_target == 0 or square_target == 1:
         print(f"The square root of {square_target} is {square_target}")
         return square_target
     
+    #设置二分查找的边界  high如果传入值大于1这是本身 如果小于1则为1
     low = 0.0
     high = square_target if square_target > 1 else 1.0
 
+    #二分查找方法
     for _ in range(max_iterations):
         mid = (low + high) / 2
         mid_squared = mid * mid 
