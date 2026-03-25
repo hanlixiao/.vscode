7aece538a11db5fffbe92f537bcb952a3cc58c64
hanlixiao <439840469@qq.com>
2026-03-25 13:40:03 +0800

    first save

--- 22.py
+++ 22.py
@@ -1,11 +1,18 @@
+#练习           归并排序
+
+
+#定义函数
 def merge_sort(array):
+    #当列表长度小于等于1时返回
     if len(array) <= 1:
         return
     
+    #将列表分成两部分
     middle_point = len(array) // 2
     left_part = array[:middle_point]
     right_part = array[middle_point:]
 
+    #递归两部分列表
     merge_sort(left_part)
     merge_sort(right_part)
 
@@ -13,6 +20,7 @@ def merge_sort(array):
     right_array_index = 0
     sorted_index = 0
 
+    #将小的放在前面
     while left_array_index < len(left_part) and right_array_index < len(right_part):
         if left_part[left_array_index] < right_part[right_array_index]:
             array[sorted_index] = left_part[left_array_index]
@@ -22,17 +30,21 @@ def merge_sort(array):
             right_array_index += 1
         sorted_index += 1
 
+    #当左边还有数时继续添加
     while left_array_index < len(left_part):
         array[sorted_index] = left_part[left_array_index]
         left_array_index += 1
         sorted_index += 1
-    
+
+    #当右边还有数时继续添加
     while right_array_index < len(right_part):
         array[sorted_index] = right_part[right_array_index]
         right_array_index += 1
         sorted_index += 1
 
 
+
+#测试
 if __name__ == '__main__':
     numbers = [4, 10, 6, 14, 2, 1, 8, 5]
     print('Unsorted array: ')
