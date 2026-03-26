a606e86fd2a63194285825f970a88439953b8398
hanlixiao <439840469@qq.com>
2026-03-26 13:34:34 +0800

    first save

---
+++ 23.py
@@ -0,0 +1,23 @@
+def quick_sort(arr):
+    # 递归终止条件：空 或 1个元素，直接返回
+    if len(arr) <= 1:
+        return arr.copy()  # 确保不修改原数组，返回新列表
+
+    # 选择第一个元素作为枢轴
+    pivot = arr[0]
+
+    # 分成三个部分：小于、等于、大于
+    less = []
+    equal = []
+    greater = []
+
+    for num in arr:
+        if num < pivot:
+            less.append(num)
+        elif num == pivot:
+            equal.append(num)
+        else:
+            greater.append(num)
+
+    # 递归 + 拼接
+    return quick_sort(less) + equal + quick_sort(greater)
