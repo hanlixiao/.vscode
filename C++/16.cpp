a36ca6bf37fb581ef98e74a590cae851eb92be0f
hanlixiao <439840469@qq.com>
2026-05-02 11:53:18 +0800

    first save

---
+++ C++/16.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution
+{
+public:
+    int removeDuplicates(vector<int>& nums)
+    {
+        if (nums.empty()) return 0;
+        int slow = 0;
+        for (int fast = 1; fast < nums.size(); ++fast)
+        {
+            if (nums[fast] != nums[slow])
+            {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+        }
+        return slow + 1;
+    }
+
+};
