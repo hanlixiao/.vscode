4745a71b9e3a781ed8f01597bf5364035a5e7582
hanlixiao <439840469@qq.com>
2026-05-08 08:21:02 +0800

    first save

--- C++/19.cpp
+++ C++/19.cpp
@@ -23,35 +23,35 @@ public:
                 return mid;
             }
 
-
-
+            // 判断左半部分是否有序
+            // 注意：这里必须包含等号，因为当left ==mid 时，也算作左边有序
             if (nums[left] <= nums[mid])
             {
-
-
-
+                // 左半部分[left，mid]是有序的
+                // 检查target是否在这个有序区间内
                 if (nums[left] <= target && target < nums[mid])
                 {
-
+                    // target 在左边，收缩右边
                     right = mid - 1;
                 }
                 else
                 {
-
+                    // target 在右边，收缩左边
                     left = mid + 1;
                 }
             }
             else
             {
-
-
+                // 右半部分[mid，right]是有序的
+                // 检查 target 是否在这个有序区间内
                 if (nums[mid] < target && target <= nums[right])
                 {
-
+                    // target 在右边，收缩左边
                     left = mid + 1;
                 }
                 else
                 {
+                    // target 在左边，收缩右边
                     right = mid - 1;
                 }
             }
