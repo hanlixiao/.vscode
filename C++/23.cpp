df691946f929f57abc16fb4e6446edd85871456f
hanlixiao <439840469@qq.com>
2026-05-12 13:29:02 +0800

    first save

--- C++/23.cpp
+++ C++/23.cpp
@@ -3,5 +3,29 @@ using namespace std;
 
 class Solution {
 public:
-    
+    string countAndSay(int n) {
+        if (n == 1) return "1";
+
+        string prev = "1";
+        for (int i = 2; i <= n; ++i) {
+            string curr = "";
+            int count = 1;
+            char say = prev[0];
+
+            // 遍历上一项，生成当前项
+            for (int j = 1; j < prev.size(); ++j) {
+                if (prev[j] == say) {
+                    count ++;
+                } else {
+                    curr += to_string(count) + say;
+                    say = prev[j];
+                    count = 1;
+                }
+            }
+
+            curr += to_string(count) + say;
+            prev = curr;
+        }
+        return prev;
+    }  
 };
