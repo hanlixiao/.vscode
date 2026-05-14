ddf73a212a2c01d777bd8dd5e3bc941f4accb7a5
hanlixiao <439840469@qq.com>
2026-05-14 13:33:59 +0800

    first save

---
+++ C++/25.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+
+    void backtracking(vector<int>& candidates, int target, int sum, int startIndex) {
+        if (sum == target) {
+            result.push_back(path);
+            return;
+        }
+
+        // 
+        for (int i = startIndex; i < candidates.size() && sum + candidates[i] <= target; i++) {
+            // 
+            // 
+            // 
+            // 
+            if (i > startIndex && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+
+
+            sum += candidates[i];
+            path.push_back(candidates[i]);
+
+
+            // 
+            backtracking(candidates, target, sum, i + 1);
+
+
+            sum -= candidates[i]; // 
+            path.pop_back(); // 
+        }
+    }
+
+
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<bool> used(candidates.size(), false);
+        sort(candidates.begin(), candidates.end());
+        backtracking(candidates, target, 0, 0);
+        return result;
+    }
+};
