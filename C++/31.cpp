2ac766460698b6b694da7519e3d966443e3768d4
hanlixiao <439840469@qq.com>
2026-05-22 13:32:03 +0800

    first save

--- C++/31.cpp
+++ C++/31.cpp
@@ -1,3 +1,10 @@
+//练习                                   字母异位词分组
+//                                给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+//                                例：输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+//                                    输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
