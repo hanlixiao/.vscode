a000beb98b3c181236043e9d399be68a26c5ff88
hanlixiao <439840469@qq.com>
2026-06-30 13:19:20 +0800

    58

--- C++/58.cpp
+++ C++/58.cpp
@@ -1,28 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
-    int numDecodings(string s) {
-        int n = s.size();
-        vector<int> dp(n + 1, 0);
-        dp[0] = 1;  // 空串基准
-        // 初始化第一个字符
-        if (s[0] != '0') dp[1] = 1;
-
-        for (int i = 2; i <= n; ++i) {
-            // 情况1：单独取当前一位
-            char cur = s[i - 1];
-            if (cur != '0') {
-                dp[i] += dp[i - 1];
-            }
-            // 情况2：取末尾两位
-            char pre = s[i - 2];
-            int two = (pre - '0') * 10 + (cur - '0');
-            if (two >= 10 && two <= 26) {
-                dp[i] += dp[i - 2];
-            }
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode dummy(0, head);
+        ListNode* pre = &dummy;
+        // 1.走到left-1位置，pre是反转区间前一个节点
+        for (int i = 1; i < left; ++i) {
+            pre = pre->next;
         }
-        return dp[n];
+        ListNode* subHead = pre->next;  // 区间第一个节点（反转后变尾）
+        ListNode* cur = subHead;
+        ListNode* prev = nullptr;
+        // 2.反转right-left+1 个节点
+        for (int i = 0; i <= right - left; ++i) {
+            ListNode* nxt = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = nxt;
+        }
+        // 3.拼接：pre连反转后的头prev，原区间头subHead连后面的cur
+        pre->next = prev;
+        subHead->next = cur;
+        return dummy.next;
     }
 };
