7a1b1dbd073a19523b06d8808ed611ef814b5106
hanlixiao <439840469@qq.com>
2026-07-28 10:48:57 +0800

    78

---
+++ C++/78.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+    int total = 0;
+    void dfs(TreeNode* node, int cur) {
+        if (!node) return;
+        cur = cur * 10 + node->val;
+        // 到达叶子节点
+        if (!node->left && !node->right) {
+            total += cur;
+            return;
+        }
+        dfs(node->left, cur);
+        dfs(node->right, cur);
+    }
+
+public:
+    int sumNumbers(TreeNode* root) {
+        total = 0;
+        dfs(root, 0);
+        return total;
+    }
+};
