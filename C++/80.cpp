2e5526a5475682f16375bc3a35c32c4153d18665
hanlixiao <439840469@qq.com>
2026-07-30 12:13:01 +0800

    80

---
+++ C++/80.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> res;  // 存放所有结果
+    vector<string> path;  // 当前正在搜索的分割方案
+
+    // 判断子串 s[1..r] 是否回文
+    bool isPalindrome(string &s, int l, int r) {
+        while(l < r) {
+            if (s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    // start：当前开始分割的下标
+    void backtrack(string &s, int start) {
+        // 递归终止：分割倒字符串末尾，找到一组方案
+        if (start == s.size()) {
+            res.push_back(path);
+            return;
+        }
+
+        // end尝试所有分割终点:[start,end]作为一段
+        for (int end = start; end < s.size(); ++end) {
+            if (isPalindrome(s, start, end)) {
+                // 选择：截取回文子串加入路径
+                path.push_back(s.substr(start, end - start + 1));
+                // 递归:下一轮从 end+1 开始分割
+                backtrack(s, end + 1);
+                // 回溯：撤销选择
+                path.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        backtrack(s, 0);
+        return res;
+    }
+};
