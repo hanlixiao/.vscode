85951505cf0e8dee2c18854a27158db6f46636f7
hanlixiao <439840469@qq.com>
2026-07-31 11:16:05 +0800

    81

---
+++ C++/81.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    vector<Node*> neightbors;
+    Node() {
+        val = 0;
+        neightbors = vector<Node*> ();
+    }
+    Node(int _val) {
+        val = _val;
+        neightbors = vector<Node*> ();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neightbors = _neighbors;
+    }
+};
+
+class Solution {
+public:
+    // key:原图节点，value:克隆出来的新节点
+    unordered_map<Node*, Node*> visited;
+
+    Node* cloneGraph(Node* node) {
+        // 边界：空节点
+        if (node == nullptr) return nullptr;
+        
+        // 如果该节点已经克隆完成，直接返回克隆节点
+        if (visited.find(node) != visited.end()) {
+            return visited[node];
+        }
+
+        // 1. 创建当前节点的克隆
+        Node* cloneNode = new Node(node->val);
+        visited[node] = cloneNode;
+
+        // 2. 遍历原节点所有邻居，递归克隆邻居并加入克隆节点邻接表
+        for (Node* neighbor : node->neightbors) {
+            cloneNode->neightbors.push_back(cloneGraph(neighbor));
+        }
+        return cloneNode;
+    }
+};
