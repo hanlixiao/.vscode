c29f87a11648161f1badfa50c2c66b30d456b893
hanlixiao <439840469@qq.com>
2026-08-04 09:55:07 +0800

    85

---
+++ C++/85.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        int n = s.size();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;  // 空串
+        
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (dp[j] && dict.count(s.substr(j, i - j))) {
+                    dp[i] = true;
+                    break;  // 找到一种拆分即可，无需继续
+                }
+            }
+        }
+        return dp[n];
+    }
+};
