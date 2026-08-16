ec82eba399753762f3960cc1b3553ed621977306
hanlixiao <439840469@qq.com>
2026-08-16 12:40:24 +0800

    最小栈

--- C++/95.cpp
+++ C++/95.cpp
@@ -1,3 +1,15 @@
+// 练习                                            最小栈
+//                                设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+//                                实现 MinStack 类:
+//                                MinStack() 初始化堆栈对象。
+//                                void push(int value) 将元素 value 推入堆栈。
+//                                void pop() 删除堆栈顶部的元素。
+//                                int top() 获取堆栈顶部的元素。
+//                                int getMin() 获取堆栈中的最小元素。
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
