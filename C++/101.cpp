741ff23145c5f9c3e4a4023d39d6572f5617856d
hanlixiao <439840469@qq.com>
2026-08-22 09:53:54 +0800

    阶乘后的零

--- C++/101.cpp
+++ C++/101.cpp
@@ -1,3 +1,10 @@
+// 练习                                   阶乘后的零
+//                                 给定一个整数 n ，返回 n! 结果中尾随零的数量。
+//                                 提示 n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
