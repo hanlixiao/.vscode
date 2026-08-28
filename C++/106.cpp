3f6aa8e757f7c8a7f8365547498c8358085f1f5d
hanlixiao <439840469@qq.com>
2026-08-28 10:47:33 +0800

    106

---
+++ C++/106.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        if (n >= 2) dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < n; ++i) {
+            dp[i] = max(dp[i - 1], dp[i - 2] +nums[i]);
+        }
+        return dp.back();
+    }
+};
