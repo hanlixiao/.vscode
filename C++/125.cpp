3e63d108f38eb231cafb57d91c8a4d982e56f9c3
hanlixiao <439840469@qq.com>
2026-09-17 13:49:07 +0800

    125

--- C++/125.cpp
+++ C++/125.cpp
@@ -1,40 +1,25 @@
-//练习                                          组合总和 III
-//                               找出所有相加之和为 n 的 k 个数的组合，且满足下列条件：
-//                               只使用数字1到9
-//                               每个数字 最多使用一次 
-//                               返回 所有可能的有效组合的列表 。该列表不能包含相同的组合两次，组合可以以任何顺序返回。
-
-
-
-
 #include <bits/stdc++.h>
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
-    vector<vector<int>> result;
-    vector<int> path;
-
-    void backtrack(int start, int k, int n, int sum) {
-        // 选够k个数字
-        if (path.size() == k) {
-            if (sum == n) {
-                result.push_back(path);
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* cur = root;
+        while (true) {
+            if (p->val < cur->val && q->val < cur->val) {
+                cur = cur->left;
+            } else if (p->val > cur->val && q->val > cur->val) {
+                cur = cur->right;
+            } else {
+                return cur;
             }
-            return;
         }
-        for(int i = start; i <= 9; ++i) {
-            if (sum + i > n) break;  // 剪枝,后面更大直接不用试
-            path.push_back(i);
-            backtrack(i = 1, k, n, sum + i);
-            path.pop_back();  // 回溯
-        }
-    }
-
-    vector<vector<int>> combinationSum3(int k, int n) {
-        result.clear();
-        path.clear();
-        backtrack(1, k, n, 0);
-        return result;
     }
 };
