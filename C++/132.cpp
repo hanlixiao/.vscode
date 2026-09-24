46b19c4f55442458d5f22da5b9915568bbe95701
hanlixiao <439840469@qq.com>
2026-09-24 12:59:13 +0800

    132

---
+++ C++/132.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> dp(n);
+        dp[0] = 1;
+        int p2 = 0, p3 = 0, p5 = 0;
+        for (int i = 1; i < n; ++i) {
+            int v2 = dp[p2] * 2;
+            int v3 = dp[p3] * 3;
+            int v5 = dp[p5] * 5;
+            dp[i] = min({v2, v3, v5});
+            if (dp[i] == v2) p2++;
+            if (dp[i] == v3) p3++;
+            if (dp[i] == v5) p5++;
+        }
+        return dp[n - 1];
+    }
+};
